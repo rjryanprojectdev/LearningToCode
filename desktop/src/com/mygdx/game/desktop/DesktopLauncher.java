1d2e173b1e232c75650796b3c0ef576456f150fc
rmclean98 <35905100+rmclean98@users.noreply.github.com>
2019-03-11 14:11:46 -0400

    test

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,7 @@ import com.mygdx.game.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		System.out.println("Hey");
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
