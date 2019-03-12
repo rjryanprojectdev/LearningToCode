d3cce21b190816b00727c7754abb727a616b340c
witschgerrj <rj.witschger@gmail.com>
2019-03-11 20:52:48 -0400

    Player images
    Player images

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,8 +7,9 @@ import com.mygdx.game.Main;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1920;
-		config.height = 1080;
+		config.resizable = false;
+		config.width = 1920/2;
+		config.height = 1080/2;
 		new LwjglApplication(new Main(), config);
 	}
 }
