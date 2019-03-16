6d51ea476bd8f4c95a162000781bf356196f5445
witschgerrj <rj.witschger@gmail.com>
2019-03-16 01:53:15 -0400

    Assets
    Assets

--- core/src/com/mygdx/game/Main.java
+++ core/src/com/mygdx/game/Main.java
@@ -1,21 +1,31 @@
 package com.mygdx.game;
 
+
+import Sprites.Player;
 import com.badlogic.gdx.ApplicationAdapter;
 
 public class Main extends ApplicationAdapter {
 
 	Map map = new Map();
+	Player player = new Player();
 	
 	@Override
 	public void create () {
-		map.create();
+        map.create();
+        player.create();
 	}
 
 	@Override
 	public void render () {
-		map.render();
+        map.render();
+        player.render();
 	}
-	
+
+	public void resize () {
+
+    }
+
+
 	@Override
 	public void dispose () {
 		map.dispose();
--- core/src/com/mygdx/game/Map.java
+++ core/src/com/mygdx/game/Map.java
@@ -10,16 +10,15 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
-public class Map extends ApplicationAdapter {
+public class Map {
 
     private TiledMap gameMap;
     private AssetManager manager;
-    private OrthographicCamera viewPort;
     private OrthogonalTiledMapRenderer renderer;
+    private OrthographicCamera viewPort;
 
-    private int TILEWIDTH, TILEHEIGHT, MAPWIDTH_TILES, MAPHEIGHT_TILES, MAPWIDTH_PIXELS, MAPHEIGHT_PIXELS;
+    private int tileWidth, tileHeight, mapWidth_Tiles, mapHeight_Tiles, mapWidth_Pixels,  mapHeight_Pixels;
 
-    @Override
     public void create () {
         manager = new AssetManager();
         manager.setLoader(TiledMap.class, new TmxMapLoader());
@@ -29,32 +28,42 @@ public class Map extends ApplicationAdapter {
         gameMap = manager.get("core/assets/map_v1.tmx", TiledMap.class);
 
         MapProperties properties = gameMap.getProperties();
-        TILEWIDTH = properties.get("tilewidth", Integer.class);
-        TILEHEIGHT = properties.get("tileheight", Integer.class);
-        MAPWIDTH_TILES = properties.get("width", Integer.class);
-        MAPHEIGHT_TILES = properties.get("height", Integer.class);
-        MAPWIDTH_PIXELS = MAPWIDTH_TILES * TILEWIDTH;
-        MAPHEIGHT_PIXELS = MAPHEIGHT_TILES * TILEHEIGHT;
-
-        viewPort = new OrthographicCamera(480.f, 270.f);
-        viewPort.position.x = MAPWIDTH_PIXELS * 0.4f;
-        viewPort.position.y = MAPHEIGHT_PIXELS * 0.85f;
+        tileWidth = properties.get("tilewidth", Integer.class);
+        tileHeight = properties.get("tileheight", Integer.class);
+        mapWidth_Tiles = properties.get("width", Integer.class);
+        mapHeight_Tiles = properties.get("height", Integer.class);
+        mapWidth_Pixels = mapWidth_Tiles * tileWidth;
+        mapHeight_Pixels = mapHeight_Tiles * tileHeight;
 
         renderer = new OrthogonalTiledMapRenderer(gameMap);
+
+
+
+        viewPort = new OrthographicCamera(960.f, 540.f);
+        viewPort.position.x = mapWidth_Pixels * 0.4f;
+        viewPort.position.y = mapHeight_Pixels * 0.85f;
     }
 
-    @Override
     public void render () {
         Gdx.gl.glClearColor(.5f, .7f, .9f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        viewPort.update();
         renderer.setView(viewPort);
         renderer.render();
+
+        viewPort.update();
     }
 
-    @Override
     public void dispose () {
         manager.dispose();
+        renderer.dispose();
+    }
+
+    public int getWidthPixels() {
+        return mapWidth_Pixels;
+    }
+
+    public int getHeightPixels() {
+        return mapHeight_Pixels;
     }
 }
