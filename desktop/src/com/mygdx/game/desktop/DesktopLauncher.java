b4ac84d978a9cc9ec1d49129f770f36ca31200fb
rmclean98 <35905100+rmclean98@users.noreply.github.com>
2019-03-16 02:34:43 -0400

    new game base files

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,9 +7,6 @@ import com.mygdx.game.Main;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.resizable = false;
-		config.width = 1920/2;
-		config.height = 1080/2;
 		new LwjglApplication(new Main(), config);
 	}
 }
