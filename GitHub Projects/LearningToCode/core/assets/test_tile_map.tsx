10ba839e88139dc53cb5c39d54794a8dd6265772
witschgerrj <rj.witschger@gmail.com>
2019-03-16 04:00:47 -0400

    Pushing Fixed IDE Changes
    Pushing Fixed IDE Changes

--- GitHub Projects/LearningToCode/core/assets/test_tile_map.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.2" name="test_tile_map" tilewidth="32" tileheight="32" tilecount="16" columns="4">
- <image source="../../tileSet/test_tile_map.png" width="128" height="128"/>
-</tileset>
--- GitHub Projects/LearningToCode/core/assets/tile_set_v1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.2" name="tile_set_v1" tilewidth="32" tileheight="32" tilecount="64" columns="8">
- <image source="../../tileSet/Test_Official_Assets/tile_set_v1.png" width="256" height="256"/>
-</tileset>
