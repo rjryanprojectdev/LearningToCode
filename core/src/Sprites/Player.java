e8db67dd6712070fe195102e5c464ec9cf147366
rmclean98 <35905100+rmclean98@users.noreply.github.com>
2019-03-17 16:04:17 -0400

    updated files

--- core/src/Sprites/Player.java
+++ core/src/Sprites/Player.java
@@ -1,65 +1,53 @@
 package Sprites;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.*;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.Array;
 
 public class Player {
 
-    private static final int FRAME_COLS = 4, FRAME_ROWS = 1;
-    Animation<TextureRegion> walkAnimation; // Must declare frame type (TextureRegion)
-    Texture walkSheet;
-    SpriteBatch spriteBatch;
+    public SpriteBatch batch;
+    public TextureAtlas textureAtlas;
+    public Animation animation;
+    public TextureRegion currentFrame;
+    private float elapsedTime = 0f;
+    private float player_x, player_y;
 
+    public void create() {
 
-    float elapsedTime;
-
-    public void create(){
-        // Load the sprite sheet as a Texture
-        walkSheet = new Texture(Gdx.files.internal("core/assets/spritesheets/player_spritesheet.png"));
-
-        // Use the split utility method to create a 2D array of TextureRegions. This is
-        // possible because this sprite sheet contains frames of equal size and they are
-        // all aligned.
-        TextureRegion[][] tmp = TextureRegion.split(walkSheet,
-                walkSheet.getWidth() / FRAME_COLS,
-                walkSheet.getHeight() / FRAME_ROWS);
-
-        // Place the regions into a 1D array in the correct order, starting from the top
-        // left, going across first. The Animation constructor requires a 1D array.
-        TextureRegion[] walkFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
-        int index = 0;
-        for (int i = 0; i < FRAME_ROWS; i++) {
-            for (int j = 0; j < FRAME_COLS; j++) {
-                walkFrames[index++] = tmp[i][j];
-            }
-        }
-
-        // Initialize the Animation with the frame interval and array of frames
-        walkAnimation = new Animation<TextureRegion>(0.3f, walkFrames);
-
-        // Instantiate a SpriteBatch for drawing and reset the elapsed animation
-        // time to 0
-        spriteBatch = new SpriteBatch();
-        elapsedTime = 0f;
-    }
-
-    public void render(){
-        elapsedTime += Gdx.graphics.getDeltaTime(); // Accumulate elapsed animation time
-
-        // Get current frame of animation for the current stateTime
-        TextureRegion currentFrame = walkAnimation.getKeyFrame(elapsedTime, true);
-        spriteBatch.begin();
-        spriteBatch.draw(currentFrame,  Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2); // Draw current frame at (50, 50)
-        spriteBatch.end();
+        batch = new SpriteBatch();
+        textureAtlas = new TextureAtlas(Gdx.files.internal("core/assets/spritesheets/player_spritesheet.atlas"));
+        Array<TextureAtlas.AtlasRegion> walkingFrames = textureAtlas.findRegions("walking");
+        animation = new Animation (1f/6f, walkingFrames, Animation.PlayMode.LOOP);
+        TextureRegion firstTexture = walkingFrames.first();
+        player_x = (Gdx.graphics.getWidth()-firstTexture.getRegionWidth())  / 2;
+        player_y = (Gdx.graphics.getHeight()-firstTexture.getRegionHeight()) / 2;
 
     }
 
-    public void dispose(){
-        spriteBatch.dispose();
-        walkSheet.dispose();
+    public void render() {
+        elapsedTime += Gdx.graphics.getDeltaTime();
+
+        currentFrame = (TextureRegion)animation.getKeyFrame(elapsedTime);
+
+        batch.begin();
+        batch.draw(currentFrame, player_x, player_y);
+        batch.end();
     }
 
+    public void dispose() {
+        batch.dispose();
+        textureAtlas.dispose();
+    }
+
+    public float getPlayerX() {
+        return player_x;
+    }
+
+    public float getPlayerY() {
+        return player_y;
+    }
 }
